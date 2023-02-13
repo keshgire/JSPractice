d5ce1523257d13c4c9dd958beb5c873917186ec3
keshgire <suraj.keshgire007@gmail.com>
2023-02-13 23:08:38 +0530

    message

---
+++ AddTwoNumber/script.js
@@ -0,0 +1,13 @@
+/*let c=10;
+let d=15;
+function add1(c,d){
+    return c+d;
+}
+console.log(add1(c,d))*/
+
+let c= prompt("Enter first number",'');
+let d=prompt("Enter second number",'');
+
+let e = parseInt(c) +parseInt (d) ;
+
+console.log("sum of two number is " +e);
---
+++ HelloProgram/script.js
@@ -0,0 +1,5 @@
+// console.log("hello")
+
+// alert("Hello")
+
+document.write("Hello JS")
